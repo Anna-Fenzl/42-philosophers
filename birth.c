74b8093292004e0ad7faadfa61c13267f1394f1b
Anna-selina Fenzl <afenzl@1-A-29.42heilbronn.de>
2022-07-08 16:27:00 +0200

    time functions

--- birth.c
+++ birth.c
@@ -6,28 +6,11 @@
 /*   By: afenzl <afenzl@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/05 21:46:30 by afenzl            #+#    #+#             */
-/*   Updated: 2022/07/08 16:25:10 by afenzl           ###   ########.fr       */
+/*   Updated: 2022/07/08 16:26:29 by afenzl           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philosophers.h"
-
-long	get_current_time_ms(void)
-{
-	struct timeval	current_time;
-
-	gettimeofday(&current_time, NULL);
-	return (current_time.tv_sec * 1000 + current_time.tv_usec / 1000);
-}
-
-void	sleep_ms(int ms)
-{
-	int	start;
-
-	start = get_current_time_ms();
-	while (start + ms > get_current_time_ms())
-		usleep(50);
-}
 
 void	*birth(void *data)
 {
